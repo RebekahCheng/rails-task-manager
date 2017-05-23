7692d2a23db84ccdf53c95fb77847063ed05e2e4
Rebekah Cheng <work@rebekahcheng.com>
2017-05-23 13:07:13 +0100

    added more routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,61 @@
 class TasksController < ApplicationController
 
-  # get all your tasks
+    # get all your tasks
+  def index
+    @tasks = Task.all
+  end
 
-  # find a task
-  # create a new task
-  # post a new task
-  # edit a task
-  # update a task
-  # delete a task
+    # find a task
+  def show
+    @task = Task.find(params[:id])
+  end
 
+    # create a new task
+  def new
+    @task = Task.new
+  end
 
+    # post a new task
+  def create
+    # create task
+    @task = Task.create(task_params)
+    # redirect to the path of where your index is (prefix of the path)
+    redirect_to tasks_path
+  end
+
+    # edit a task
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+    # update a task
+  def update
+    # find the task you want to update
+    @task = Task.find(params[:id])
+    # update your task using the task params
+    @task.update(task_params)
+    # redirect to the task page
+    redirect_to tasks_path(@task)
+  end
+
+    # delete a task
+  def destroy
+    # find the task you want to destroy
+    @task = Task.find(params[:id])
+    # delete task
+    @task.destroy
+    # redirect to main tasks page
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :details)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,23 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # get all the tasks
+  # get url.com/tasks to tasks controller index action
+  get 'tasks', to: 'tasks#index'
 
   # find one task
+  # get url.com/tasks/:id to tasks controller show action
+  get 'tasks/:id', to: 'tasks#show'
 
   # add a new task to the app
-  # get
+  # get url.com/new to tasks controller new action
+  get 'tasks/new', to: 'tasks#new'
   # post
+  post 'tasks', to: 'tasks#create'
+
 
   # update the info of a given task
   # get
+
   # patch
 
   # delete a task
