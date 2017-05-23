49d6c0c88712a0ff027d8341afcaa1cc499f8547
Rebekah Cheng <work@rebekahcheng.com>
2017-05-23 11:54:58 +0100

    rails new task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,14 @@
+class TasksController < ApplicationController
+
+  # get all your tasks
+
+  # find a task
+  # create a new task
+  # post a new task
+  # edit a task
+  # update a task
+  # delete a task
+
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get all the tasks
+
+  # find one task
+
+  # add a new task to the app
+  # get
+  # post
+
+  # update the info of a given task
+  # get
+  # patch
+
+  # delete a task
+
+
+end
